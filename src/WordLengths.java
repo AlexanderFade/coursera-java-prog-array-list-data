7b16d993cc60df0406aab7b30c6253c8858d6a13
Alexander Kovalevsky <alexanderfade@gmail.com>
2015-11-23 11:29:17 +0300

    added quiz txt files

---
+++ src/WordLengths.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+/**
+ * Created by Alexander Kovalevsky on 22.11.2015.
+ */
+public class WordLengths {
+    public void countWordLengths(File file, int[] counts) {
+
+    }
+
+
+}
